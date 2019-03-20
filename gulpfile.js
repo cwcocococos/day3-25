704896358ad18e6abb29942775ee770ce73ba9b0
cwc <cwc@xx.com>
2019-03-20 15:19:39 +0800

    day33

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+//导入所需插件 
+const gulp = require('gulp'),
+	  uglify = require('gulp-uglify'),
+	  rename = require('gulp-rename'),
+	  concat = require('gulp-concat'),
+	  img = require('gulp-imagemin'),
+	  cssnano = require('gulp-cssnano'),
+	  html = require('gulp-minify-html'),
+	  sass = require('gulp-sass');
+	  
+//任务1：编译sass压缩css
+gulp.task('sass',function(){
+	gulp.src('src/sass/*')
+	.pipe(sass())
+	.pipe(cssnano())
+	.pipe(gulp.dest('dist/css'))
+});
+//任务2：处理js任务
+gulp.task('js',function(){
+	gulp.src('src/js/*.js')
+	.pipe(uglify())
+	.pipe(gulp.dest('dist/js'));
+})
+//任务3：压缩html
+gulp.task('html',function(){
+	gulp.src('src/html/*')
+	.pipe(html())
+	.pipe(gulp.dest('dist/html'))
+});
+//任务4：压缩图片
+gulp.task('image',function(){
+	gulp.src('src/img/*')
+	.pipe(img())
+	.pipe(gulp.dest('dist/img'))
+});
+//发布自动监听任务
+gulp.task('default',function(){
+	gulp.watch('src/sass/*',['sass']);
+	gulp.watch('src/js/*',['js']);
+	gulp.watch('src/html/*',['html']);
+	gulp.watch('src/image/*',['image']);
+})
---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+$(function(){
+	//吸顶效果
+	$(window).scroll(function(){
+		var sTop = $(document).scrollTop();
+		if(sTop > 100){
+			$('#topNav').show(1000);
+			$('#topNav').css("position","fixed");
+			$('#topNav').css("top",0)
+		}else{
+			$('#topNav').css("display","none");
+		}
+	})
+	
+	//轮播
+	var timer = setInterval(auto,1500);
+	var index = 0;
+	function auto(){
+		index++;
+		if( index == $("ol li").size() ){
+			index = 0;
+		}
+		$("ol li").eq(index).addClass("current").siblings().removeClass("current");
+		$(".pic li").eq(index).fadeIn(1000).siblings().fadeOut(1000);
+	}
+	
+	//鼠标 操作
+	$("ol li").mouseenter(function(){
+		clearInterval(timer);
+		index = $(this).index()-1;
+		auto();
+	})
+	$("ol li").mouseout(function(){
+		timer = setInterval(auto,1500);
+	})
+
+})
